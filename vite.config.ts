a37862af9f4209b71c5d6e6223fbde14cf1bfec9
Sébastien Arod <sebastien.arod@gmail.com>
2025-10-26 21:21:42 +0100

    ci: deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/book-scanner/",
   plugins: [react()],
   server: {
     allowedHosts: ["97b70403687b.ngrok-free.app"],
