88d1e3b8d124630ab69efde7af50c00d4caa0f7f
Sébastien Arod <sebastien.arod@gmail.com>
2025-11-21 13:33:31 +0100

    feat(i18n): setup i18n

---
+++ src/i18n-resources.ts
@@ -0,0 +1,27 @@
+export const i18nResources = {
+  en: {
+    translation: {
+      // App titles and general
+      'app.title': 'Book Scanner App',
+
+      // Book list related
+      'booklist.item_title.header': 'Book List',
+      'booklist.match_status.header': 'Match Status',
+      'booklist.item_title.header_header': 'Title',
+      'booklist.match_status.matched_book': 'Matched Book',
+      'booklist.match_status.unmatched_library_book': 'Unmatched Library Book',
+      'booklist.match_status.unmatched_isbn_book': 'Scanned but not in list',
+      'booklist.match_status.fetch_error': 'Failed to fetch book data for isbn',
+      'booklist.return_state.header': 'Ontime/Overdue Status',
+      'booklist.return_state.on_time': 'On time',
+      'booklist.return_state.overdue': 'Overdue',
+      'booklist.return_state.no_data': '-',
+
+      // Actions
+      'action.reset': 'Reset',
+      'action.start_scanning': 'Start Scanning',
+      'action.stop_scanning': 'Stop Scanning',
+      'action.upload': 'Upload book list',
+    },
+  },
+};
